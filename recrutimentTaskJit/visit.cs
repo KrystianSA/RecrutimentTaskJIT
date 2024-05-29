19d3d298059b6e5f479b7f5d7429ca27d23e515b
KrystianSA <122701689+KrystianSA@users.noreply.github.com>
2024-05-29 14:35:10 +0200

    Result

---
+++ recrutimentTaskJit/visit.cs
@@ -0,0 +1,9 @@
+public class Visit
+{
+    public required int id { get; set; }
+    public required DateTime date { get; set; }
+    public required string personName { get; set; }
+    public required string catName { get; set; }
+    public required int age { get; set; }
+    public required string color { get; set; }
+}
